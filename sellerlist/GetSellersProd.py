9b4d030e9399d7b6db9cd798cbd0924c27641984
Satya Prahlad <prahald1989@gmail.com>
2020-05-01 14:37:10 +0530

    adding requirments

--- sellerlist/GetSellersProd.py
+++ sellerlist/GetSellersProd.py
@@ -1,13 +1,11 @@
 from ebaysdk.trading import Connection as Trading
-from ebaysdk.exception import ConnectionError
-import json
 import datetime
 import time
 import gspread.client
 from oauth2client.service_account import ServiceAccountCredentials
 
 
-def updateToGSheet( data):
+def updateToGSheet( data,error=None):
     scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
              "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
 
@@ -19,8 +17,14 @@ def updateToGSheet( data):
 
     eachRow1 = ['Title', 'Price', 'Watch',
                'Sold', 'CategoryID',
-               'Duration']
+               'Duration','','Last Updated at: '+str(datetime.datetime.now())]
     #heading
+    if (error is not None):
+        errors=['Failed to update sheet with reason : ',str(error),' at ',str(datetime.datetime.now())]
+        print("error with ",error)
+        sheet1.clear()
+        sheet1.append_row(errors)
+        return
 
 
     #sheet1.append_row(eachRow1)
@@ -33,6 +37,7 @@ def updateToGSheet( data):
         print(eachItem)
         eachRow = [eachItem['Title'], float(eachItem['SellingStatus']['CurrentPrice']['value']), int(eachItem['WatchCount']),
                    int(eachItem['SellingStatus']['QuantitySold']), int(eachItem['PrimaryCategory']['CategoryID']), eachItem['ListingDuration']]
+
         allRowsValues.append(eachRow)
 
     #print("all rows are ",allRowsValues)
@@ -46,6 +51,12 @@ def updateToGSheet( data):
         "blue": 0.0
     }}})
 
+    sheet1.format("H1:H1", {"textFormat": {"bold": True, "fontSize": 12, "foregroundColor": {
+        "red": 0.0,
+        "green": 1.0,
+        "blue": 0.0
+    }}})
+
     #
     # sheet1.format("A"+str(len(allRowsValues))+":F"+str(len(allRowsValues)), {"textFormat": {"bold": False, "fontSize": 10, "foregroundColor": {
     #     "red": 0.0,""
@@ -56,12 +67,13 @@ def updateToGSheet( data):
 
 
 def main():
+    try:
 
-    api = Trading(config_file=None,domain='api.ebay.com',appid="SatyaPra-MyEBPrac-PRD-abce464fb-dd2ae5fe", devid="6c042b69-e90f-4897-9045-060858248665",
+        api = Trading(config_file=None,domain='api.ebay.com',appid="SatyaPra-MyEBPrac-PRD-abce464fb-dd2ae5fe", devid="6c042b69-e90f-4897-9045-060858248665",
               certid="AgAAAA**AQAAAA**aAAAAA**zY2pXg**nY+sHZ2PrBmdj6wVnY+sEZ2PrA2dj6MElIuoC5CEpAydj6x9nY+seQ**/0kGAA**AAMAAA**7/kuig0GXA6zyrQ/D6lHcXQVtH2z/rLeU4EzvA4jqRnDtBEXlUMdK5ZuE0EpkrfiI7bUcY6xVnn9C/6Iajfk9uhKoayDerpacV7wwRbELehUna4jXa57klixCENuF6UbPprzmnbIVt0pdoWNw5Yfa9dSEZb6FKw4eVwWwwABNUOIxd7ozBMVoZwywBQ5JwOKl2U8wzfnxzNH/tiSn5NSkiqOJztVlOT5XZWPB1wlaoZsLrGBxZDEeAQ3C+fKOm5NJIpEn6YCZF7u0SPulv2I4gcXCJVUQ8en7bClXajs4c/Qfo+gelpYHKKeTWVInQbqcEF+OoUcA0deqjYgIxsJ6J0iRXQoaESYV1tZ1zLVvNcxrysuwVpV7Iwjs5alfKQmAgVlBFjstbi3Pgr/NFR3s3srGR6DLoBNfF6w2yP6LE7+KVK+rxbzz8hE0yAJ06WpOrgJmmkZd0jxaLlImTsNHB2CxmwkWimoEEwwq/DirLhNxS4SHs34LMxeoRVOxDG39VrQcDe0q+18aCX2SPHRmBcGvcV4x+a6JqwhenMVZiKypQq/RQpeD7PeVSQAh1Vd3zvRB8w1zvoUAHmL9JWCfrnwe2fVDBr/DfE9IpPmYNCbvLhpxAMMYMMS0RzwPCpriCdleJi7wBqKJ/jseQOndNWb028A8jnWzXPMkgbF+P5sj+Tu48XY56E7Oe1NmynMg0EswO3PwdFd9Cevdkbn6CbuSGloK2JQ3P28rnRjzh8LEdYSR+iL3mMpuR7N9BgF")
 
-    items=list()
-    inputObj={ "RequesterCredentials": {
+        items=list()
+        inputObj={ "RequesterCredentials": {
                                "eBayAuthToken": "AgAAAA**AQAAAA**aAAAAA**zY2pXg**nY+sHZ2PrBmdj6wVnY+sEZ2PrA2dj6MElIuoC5CEpAydj6x9nY+seQ**/0kGAA**AAMAAA**7/kuig0GXA6zyrQ/D6lHcXQVtH2z/rLeU4EzvA4jqRnDtBEXlUMdK5ZuE0EpkrfiI7bUcY6xVnn9C/6Iajfk9uhKoayDerpacV7wwRbELehUna4jXa57klixCENuF6UbPprzmnbIVt0pdoWNw5Yfa9dSEZb6FKw4eVwWwwABNUOIxd7ozBMVoZwywBQ5JwOKl2U8wzfnxzNH/tiSn5NSkiqOJztVlOT5XZWPB1wlaoZsLrGBxZDEeAQ3C+fKOm5NJIpEn6YCZF7u0SPulv2I4gcXCJVUQ8en7bClXajs4c/Qfo+gelpYHKKeTWVInQbqcEF+OoUcA0deqjYgIxsJ6J0iRXQoaESYV1tZ1zLVvNcxrysuwVpV7Iwjs5alfKQmAgVlBFjstbi3Pgr/NFR3s3srGR6DLoBNfF6w2yP6LE7+KVK+rxbzz8hE0yAJ06WpOrgJmmkZd0jxaLlImTsNHB2CxmwkWimoEEwwq/DirLhNxS4SHs34LMxeoRVOxDG39VrQcDe0q+18aCX2SPHRmBcGvcV4x+a6JqwhenMVZiKypQq/RQpeD7PeVSQAh1Vd3zvRB8w1zvoUAHmL9JWCfrnwe2fVDBr/DfE9IpPmYNCbvLhpxAMMYMMS0RzwPCpriCdleJi7wBqKJ/jseQOndNWb028A8jnWzXPMkgbF+P5sj+Tu48XY56E7Oe1NmynMg0EswO3PwdFd9Cevdkbn6CbuSGloK2JQ3P28rnRjzh8LEdYSR+iL3mMpuR7N9BgF"
                            },
                            "ErrorLanguage": "en_US",
@@ -93,39 +105,41 @@ def main():
                            }
                        }
 
-    startDateTo =datetime.datetime.now()
-    startDateFrom =startDateTo-datetime.timedelta(90)
+        startDateTo =datetime.datetime.now()
+        startDateFrom =startDateTo-datetime.timedelta(90)
 
-    for i in range(5):
-        inputObj["StartTimeTo"] = startDateTo
-        inputObj["StartTimeFrom"] = startDateFrom
-        inputObj["Pagination"]["PageNumber"]=1
-        print("iteration number ", i)
-        print(inputObj["StartTimeTo"], "  ", inputObj["StartTimeFrom"])
-        while True:
+        for i in range(5):
+            inputObj["StartTimeTo"] = startDateTo
+            inputObj["StartTimeFrom"] = startDateFrom
+            inputObj["Pagination"]["PageNumber"]=1
+            print("iteration number ", i)
+            print(inputObj["StartTimeTo"], "  ", inputObj["StartTimeFrom"])
+            while True:
             #print(inputObj)
 
-            print('\n')
-            response = api.execute('GetSellerList',inputObj).dict()
-            if response["ItemArray"] is None:
-                print("no result at i ",i)
-                break
-            currentItems=response["ItemArray"]["Item"]
-            items.extend(currentItems)
-            print("page number is ",response["PageNumber"])
-            print("remaining pages",response["PaginationResult"])
-            print("has more number ",response["HasMoreItems"])
-            if response["HasMoreItems"]!="true":
-                break
-            inputObj["Pagination"]["PageNumber"] = int(response["PageNumber"]) + 1
-        if i==3:
-            startDateFrom = startDateFrom - datetime.timedelta(6) #just for last 6 days in 365/366  days
-        else:
-            startDateFrom=startDateFrom-datetime.timedelta(90)
-        startDateTo = startDateTo - datetime.timedelta(90)
+                print('\n')
+                response = api.execute('GetSellerList',inputObj).dict()
+                if response["ItemArray"] is None:
+                    print("no result at i ",i)
+                    break
+                currentItems=response["ItemArray"]["Item"]
+                items.extend(currentItems)
+                print("page number is ",response["PageNumber"])
+                print("remaining pages",response["PaginationResult"])
+                print("has more number ",response["HasMoreItems"])
+                if response["HasMoreItems"]!="true":
+                    break
+                inputObj["Pagination"]["PageNumber"] = int(response["PageNumber"]) + 1
+            if i==3:
+                startDateFrom = startDateFrom - datetime.timedelta(6) #just for last 6 days in 365/366  days
+            else:
+                startDateFrom=startDateFrom-datetime.timedelta(90)
+            startDateTo = startDateTo - datetime.timedelta(90)
 
-    print(items,file=open("1.txt","w"))
-    updateToGSheet(items)
+        print(items,file=open("1.txt","w"))
+        updateToGSheet(items)
+    except Exception as error:
+        updateToGSheet(None,error=error)
 
 #print(json.dumps(response,indent=1),file=open("1.txt","w"))
 #print(int(time.time())-int(startTime))
